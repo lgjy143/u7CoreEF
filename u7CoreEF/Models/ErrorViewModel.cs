d1a4dba117dfff9e8d24937d19b6b8b9d5c9838d
u724 <lgjy143@163.com>
2018-04-19 10:11:33 +0800

    添加项目文件。

---
+++ u7CoreEF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace u7CoreEF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
