fd5d480156fba390a318faab8a4d708506d8a397
u724 <lgjy143@163.com>
2018-04-19 17:24:10 +0800

    init

---
+++ u7CoreEF/Migrations/20180419031940_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace u7CoreEF.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SysUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    PassWord = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SysUser", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SysUser");
+        }
+    }
+}
---
+++ u7CoreEF/Models/SysUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace u7CoreEF.Models
+{
+    public class SysUser
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string Email { get; set; }
+    }
+}
